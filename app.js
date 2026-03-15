c83bb9a17c52a0608c9d4a341ae4ce82162e4b8c
옴니이솔1부문-\78als <78alswo@douzone.com>
2026-03-15 15:15:14 +0900

    users 전역변수로 빼기 및 cors이슈 라이브러리 추가

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
+
+const users = [
+    { id: 1, name: '철수'},
+    { id: 2, name: '영희'},
+    { id: 3, name: '기모찌'}
+];
 
 app.get('/', (req, res) => {
     res.send('서버가 작동중입니다.');
 });
 
 app.get('/users', (req, res) => {
-    const users = [
-        { id: 1, name: '철수'},
-        { id: 2, name: '영희'}
-    ];
     res.json(users);
 });
 
 app.post('/users', (req, res) => {
-    const newUser = req.body; // 사용자가 보낸 데이터가 담기는 곳
-    console.log(`newUser`);
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name
+    }; // 사용자가 보낸 데이터가 담기는 곳
+
+    users.push(newUser);
 
     res.status(201).json({
         message: '유저가 성공적으로 생성되었습니다.',
